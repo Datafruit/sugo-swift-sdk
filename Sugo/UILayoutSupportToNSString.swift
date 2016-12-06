506c983ef1a7124a613c93919a197ad35a108994
Zackx <zackx@foxmail.com>
2016-12-06 20:35:22 +0800

    Fix bug: Server cannot get the length of edgesForExtendedLayout (topLayoutGuide & bottomLayoutGuide) in UIViewController

---
+++ Sugo/UILayoutSupportToNSString.swift
@@ -0,0 +1,32 @@
+//
+//  UILayoutSupportToNSString.swift
+//  Sugo
+//
+//  Created by Zack on 6/12/16.
+//  Copyright © 2016年 sugo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@objc(UILayoutSupportToNSString) class UILayoutSupportToNSString: ValueTransformer {
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSDictionary.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return false
+    }
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let value = value as? UILayoutSupport else {
+            return nil
+        }
+        let uiLayoutSupport = value
+        uiLayoutSupport.length
+        
+        return "\(uiLayoutSupport.length)"
+    }
+}
+
