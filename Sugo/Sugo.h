09d39fcfb0493efc386e5e66a74b13c6bc99ea4a
Zackx <zackx@foxmail.com>
2016-12-28 15:46:18 +0800

    Update module header file

---
+++ Sugo/Sugo.h
@@ -0,0 +1,19 @@
+//
+//  Sugo.h
+//  Sugo
+//
+//  Created by Zack on 6/12/16.
+//  Copyright © 2016年 Sugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sugo.
+FOUNDATION_EXPORT double SugoVersionNumber;
+
+//! Project version string for Sugo.
+FOUNDATION_EXPORT const unsigned char SugoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sugo/PublicHeader.h>
+
+
