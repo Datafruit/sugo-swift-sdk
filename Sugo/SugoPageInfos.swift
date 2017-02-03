cf4d100f1d505692c88f6e37f7f84a3331c6e305
Zackx <zackx@foxmail.com>
2017-02-03 19:28:09 +0800

    Add SugoPageInfos.swift

---
+++ Sugo/SugoPageInfos.swift
@@ -0,0 +1,13 @@
+//
+//  SugoPageInfos.swift
+//  Sugo
+//
+//  Created by Zack on 3/2/17.
+//  Copyright © 2017年 sugo. All rights reserved.
+//
+
+import UIKit
+
+class SugoPageInfos: NSObject {
+
+}
