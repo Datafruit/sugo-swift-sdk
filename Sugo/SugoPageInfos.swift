159c050be6babdc843c253209fd91c5375928d9f
Zackx <zackx@foxmail.com>
2017-02-03 19:28:54 +0800

    Update SugoPageInfos.swift

--- Sugo/SugoPageInfos.swift
+++ Sugo/SugoPageInfos.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class SugoPageInfos: NSObject {
 
+    var infos: [[String: String]]
+    
+    static var global: SugoPageInfos {
+        return singleton
+    }
+    private static let singleton = SugoPageInfos()
+    
+    private override init() {
+        self.infos = [[String: String]]()
+        super.init()
+    }
+    
+    deinit {
+        self.infos.removeAll()
+    }
+
+    
 }
