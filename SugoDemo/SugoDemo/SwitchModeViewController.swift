cee5b629b4380612c5c932afb269571bd663d85f
Zack <zackx@foxmail.com>
2017-05-09 17:37:17 +0800

    Modify SugoDemo

--- SugoDemo/SugoDemo/SwitchModeViewController.swift
+++ SugoDemo/SugoDemo/SwitchModeViewController.swift
@@ -12,11 +12,21 @@ import Sugo
 class SwitchModeViewController: UIViewController {
 
     var urlString: String?
+    var type: String?
+    @IBOutlet weak var switchLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.type = handleURL()
+        if self.type == "heatmap" {
+            self.switchLabel.text = "切换至热图模式"
+        } else if self.type == "codeless" {
+            self.switchLabel.text = "切换至可视化埋点模式"
+        } else {
+            self.switchLabel.text = "信息错误，请重新扫码"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,14 +37,54 @@ class SwitchModeViewController: UIViewController {
     @IBAction func confirm(_ sender: UIButton) {
         
         print("url: \(self.urlString.debugDescription)")
-        if let url = URL(string: self.urlString!) {
-            Sugo.mainInstance().connectToCodeless(via: url)
-            _ = self.navigationController?.popToRootViewController(animated: true)
+        guard let string = self.urlString, let url = URL(string: string) else {
+            return
         }
+        
+        if self.type == "heatmap" {
+            Sugo.mainInstance().requestForHeatMap(via: url)
+            self.navigationController?.popToRootViewController(animated: true)
+        } else if self.type == "codeless" {
+            Sugo.mainInstance().connectToCodeless(via: url)
+            self.navigationController?.popToRootViewController(animated: true)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+        
     }
     
     @IBAction func cancel(_ sender: UIButton) {
         _ = self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    private func handleURL() -> String {
+        
+        print("url: \(self.urlString.debugDescription)")
+        guard let string = self.urlString,
+            let url = URL(string: string),
+            let query = url.query else {
+                return ""
+        }
+        
+        var querys = [String: String]()
+        
+        let items = query.components(separatedBy: "&")
+        for item in items {
+            let q = item.components(separatedBy: "=")
+            if q.count != 2 {
+                continue
+            }
+            querys[q.first!] = q.last!
+        }
+        
+        if let type = querys["type"],
+            let _ = querys["sKey"],
+            type == "heatmap" {
+            return "heatmap"
+        } else if querys["sKey"] != nil {
+            return "codeless"
+        }
+        return ""
     }
     
 }
