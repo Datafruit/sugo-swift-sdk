809f3edc733c5e747bef83bb7f5f7c7b260e74d9
chenyy <2543030051@qq.com>
2019-04-18 16:34:07 +0800

    package for heatmap

--- Sugo/SugoInstance.swift
+++ Sugo/SugoInstance.swift
@@ -251,8 +251,6 @@ open class SugoInstance: CustomDebugStringConvertible, FlushDelegate, CacheDeleg
     
     private func setupListeners() {
         
-        buildApplicationMoveEvent()
-        
         if SugoPermission.canTrackNativePage {
             trackStayTime()
         }
@@ -302,112 +300,6 @@ open class SugoInstance: CustomDebugStringConvertible, FlushDelegate, CacheDeleg
 
     deinit {
         NotificationCenter.default.removeObserver(self)
-    }
-    
-    private func buildApplicationMoveEvent(){
-        let sendEventBlock = {
-            [unowned self] (viewController: AnyObject?, command: Selector, param1: AnyObject?, param2: AnyObject?) in
-            guard let app = viewController as? UIApplication else {
-                return
-            }
-            
-            guard let event = param1 as? UIEvent else{
-                return
-            }
-//            application: AnyObject?, command: AnyObject?, event:UIEvent
-            let keys = SugoDimensions.keys
-            let values = SugoDimensions.values
-//            NSSet *touches = [event allTouches];
-            let touches:Set<UITouch> = event.allTouches!
-            for touch in touches {
-                switch(touch.phase){
-                case UITouch.Phase.began:
-                    
-                    let point:CGPoint = touch.location(in: UIApplication.shared.keyWindow)
-                    let x:Float = Float(point.x)
-                    let y:Float = Float(point.y)
-                    let serialNum:NSInteger = self.calculateTouchArea(x: x, y: y)
-                    
-                    var p = Properties()
-                    let userDefaults = UserDefaults.standard
-                    let pagePath = userDefaults.string(forKey: Sugo.CURRENTCONTROLLER)
-                    p[keys["PagePath"]!] = pagePath;
-                    p[keys["OnclickPoint"]!] = "\(serialNum)"
-                    if let vc = UIViewController.sugoCurrentUIViewController() {
-                        p[keys["PagePath"]!] = NSStringFromClass(vc.classForCoder)
-                        for info in SugoPageInfos.global.infos {
-                            if let infoPage = info["page"] as? String,
-                                infoPage == NSStringFromClass(vc.classForCoder) {
-                                p[keys["PageName"]!] = info["page_name"] as? String
-                                if let infoPageCategory = info["page_category"] as? String {
-                                    p[keys["PageCategory"]!] = infoPageCategory;
-                                }
-                                break
-                            }
-                        }
-                    }
-                    
-                    
-                    self.track(eventName: values["ScreenTouch"]!, properties: p)
-                    break
-                default:
-                    break
-                }
-            }
-        }
-        Swizzler.swizzleSelector(#selector(UIApplication.sendEvent(_ :)),
-                                 withSelector: #selector(UIApplication.sugoSendEventBlock(_ :)),
-                                 for: UIApplication.self,
-                                 name: UUID().uuidString,
-                                 block: sendEventBlock)
-    }
-    
-    
-    
-    
-    
-    private func calculateTouchArea(x:Float,y:Float) ->NSInteger{
-        let columnNum:Float = 36
-        let lineNum:Float = 64
-        var areaWidth:Float
-        var areaHeight:Float
-        let fullScreenH:Float = Float(UIScreen.main.bounds.size.height)
-        let fullScreenW:Float = Float(UIScreen.main.bounds.size.width)
-        var newY:Float = y
-        if fullScreenH>fullScreenW {
-            areaWidth = fullScreenW/columnNum
-            areaHeight = fullScreenH/lineNum
-        }else{//Landscape situation
-            let ratio = (fullScreenH/columnNum)/(fullScreenW/lineNum)
-            areaWidth = fullScreenW/columnNum
-            areaHeight = areaWidth*ratio
-            var statusheight:Float = 20
-            if UIScreen.main.bounds.height == 812{
-                statusheight = 44
-            }
-            let statusBarRatioHeight:Float = areaHeight/((fullScreenW/lineNum)/statusheight)
-            newY = y + statusBarRatioHeight
-        }
-        let columnSerialValue:Float = x/areaWidth
-        let lineNumSerialValue:Float = newY/areaHeight
-        var columnSerialNum:Int
-        if (columnSerialValue-Float(Int(columnSerialValue)))>0{
-            columnSerialNum = (Int(columnSerialValue)+1)
-        }else{
-            columnSerialNum = Int(columnSerialValue)
-        }
-        var lineNumSerialNum:Int
-        if(lineNumSerialValue-Float(Int(lineNumSerialValue)))>0{
-            lineNumSerialNum = Int(lineNumSerialValue)
-        }else{
-            lineNumSerialNum = Int(lineNumSerialValue)-1
-        }
-        
-        if columnSerialValue == 0{
-            columnSerialNum = columnSerialNum + 1
-        }
-        
-        return columnSerialNum + lineNumSerialNum*Int(columnNum)
     }
     
     @objc private func applicationDidBecomeActive(_ notification: Notification) {
--- sugo-swift-sdk.podspec
+++ sugo-swift-sdk.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |spec|
   spec.name                  = 'sugo-swift-sdk'
   spec.module_name           = 'Sugo'
-  spec.version               = '4.2.0'
+  spec.version               = '4.2.0.1'
   spec.license               = 'Apache License, Version 2.0'
   spec.summary               = 'Official Sugo Swift SDK for iOS (Swift)'
   spec.homepage              = 'https://github.com/Datafruit/sugo-swift-sdk'
@@ -21,5 +21,10 @@ Pod::Spec.new do |spec|
     weex.source_files   = 'Sugo/Weex/*.{m,h,swift}'
     weex.dependency 'sugo-swift-sdk/core'
     weex.dependency 'WeexSDK'
+  end
+
+  spec.subspec 'heatmap' do |heatmap|
+    heatmap.source_files   = 'Sugo/HeatMap/*.{m,h,swift}'
+    heatmap.dependency 'sugo-swift-sdk/core'
   end
 end
